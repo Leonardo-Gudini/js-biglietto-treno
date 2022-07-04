84012400888eda6007023e391309b3e8196bf333
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-04 16:07:22 +0200

    aggiunta variabile età e prezzo biglietto e applicazione sconto

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-const chilometri = parseInt(prompt("Inserisci quanti chilometri devi percorrere"));
-const eta = parseInt(prompt("Inserisci la tua età"));
+const chilometri = parseFloat(prompt("Inserisci quanti chilometri devi percorrere"));
+const eta = parseFloat(prompt("Inserisci la tua età"));
 
 const prezzoBiglietto = parseInt(0.21 * chilometri);
+
+console.log(prezzoBiglietto)
+
+if(eta < 18){
+
+    let sconto20 = prezzoBiglietto / 100 * 20;
+    alert("Il prezzo del tuo biglietto è " + ((prezzoBiglietto - sconto20) * 100 / 100).toFixed(2));
+
+} else if (eta >=65) {
+
+    let sconto40 = prezzoBiglietto / 100 * 40;
+    alert("Il prezzo del tuo biglietto è " + ((prezzoBiglietto - sconto40) * 100 / 100).toFixed(2));
+    
+} else {
+
+    alert("Il prezzo del tuo biglietto è " + prezzoBiglietto);
+}
